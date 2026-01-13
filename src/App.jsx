5daf5e2ddc9797e99be77eb304ba816204c3282b
KingJepy <jmoslander15@gmail.com>
2026-01-13 14:51:56 -0600

    started adding some html fot the homepage and inserted a photo of me and my fiancee

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
+import Homepage from './components/Homepage'
 
 function App() {
   return (
     <BrowserRouter>
 
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route path='/' element={<Homepage />} />
       </Routes>
     
     </BrowserRouter>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,3 +4,20 @@ Id like to include the date, location and welcome message.
 There should also be a button to RSVP
 */
 
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import homePageImage from '../assets/homePageImage.jpg';
+
+function Homepage() {
+    return (
+        <div className="homePage">
+            <h1>Welcome to Our Wedding Website</h1>
+            <img src={homePageImage} alt="Leeah and Joe" />
+            <h2>Our Day: September 18th, 2027</h2>
+            <h2>Location: The Hangar In Greenville</h2>
+        </div>
+    )
+}
+
+export default Homepage;
