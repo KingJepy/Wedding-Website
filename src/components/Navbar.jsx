0959896e083fd2a11428d913a4109cf020d9bed7
KingJepy <jmoslander15@gmail.com>
2026-01-15 17:31:54 -0600

    added hamburger menu to the navbar so that when on mobile the links still look decent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
+
+    // add state to impement a mobile hamburger menu
+    const [menuOpen, setMenuOpen] =useState(false);
+
     return (
         <nav className="navbar">
             <h1 className="nav-title">Leeah & Joe</h1>
 
-            <div className="nav-links">
-                <NavLink to="/" end>Home</NavLink>
-                <NavLink to="/details">Details</NavLink>
-                <NavLink to="/rsvp">RSVP</NavLink>
-                <NavLink to="/registry">Registry</NavLink>
+            {/* hamburger button for mobile */}
+            <button 
+                className="hamburger"
+                onClick={() => setMenuOpen(!menuOpen)}
+                aria-label="Toggle Nav Menu"
+            >
+                ☰
+            </button>
+
+            {/* nav links */}
+            <div className={`nav-links ${menuOpen ? "open" : ''}`}>
+                <NavLink to="/" end onClick={() => setMenuOpen(false)}>Home</NavLink>
+                <NavLink to="/details" onClick={() => setMenuOpen(false)}>Details</NavLink>
+                <NavLink to="/rsvp" onClick={() => setMenuOpen(false)}>RSVP</NavLink>
+                <NavLink to="/registry" onClick={() => setMenuOpen(false)}>Registry</NavLink>
             </div>
         </nav>
     );
