ba2843b9005cfc558aa68a9cbd2b6ea7200bf274
KingJepy <jmoslander15@gmail.com>
2026-02-06 16:31:19 -0600

    added the info for the main user of the form

--- src/components/Rsvp.jsx
+++ src/components/Rsvp.jsx
@@ -7,7 +7,10 @@ function Rsvp() {
     const [showForm, setShowForm] = useState(false);
 
     // main guest info 
-    
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [vegetarian, setVegetarian] = useState(false);
+    const [songRequest, setSongRequest] = useState("");
 
 
     return (
@@ -24,9 +27,60 @@ function Rsvp() {
             )}
 
             {showForm && (
-                <div className="rsvp-form">
-                    <p>rsvp form will go here</p>
-                </div>
+                <form className="rsvp-form">
+                    <h2>Your RSVP Information</h2>
+
+                    <label>
+                        Name
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </label>
+
+                    <label>
+                        Email
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </label>
+
+                    <div className="radio-group">
+                        <p>Dietary Preference</p>
+
+                        <label>
+                            <input
+                                type="radio"
+                                name="diet"
+                                checked={!vegetarian}
+                                onChange={() => setVegetarian(false)}
+                            />
+                            Not Vegetarian
+                        </label>
+
+                        <label>
+                            <input
+                                type="radio"
+                                name="diet"
+                                checked={vegetarian}
+                                onChange={() => setVegetarian(true)}
+                            />
+                            Vegetarian
+                        </label>
+                    </div>
+
+                    <label>
+                        Song Request
+                        <input
+                            type="text"
+                            value={songRequest}
+                            onChange={(e) => setSongRequest(e.target.value)}
+                        />
+                    </label>
+                </form>
             )}
         </div>
     );
