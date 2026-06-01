a856e626257b129738d375ecdf95cdd324a61fef
KingJepy <jmoslander15@gmail.com>
2026-06-01 18:28:25 -0500

    added css and my route for carousel

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Details from './components/Details'
 import Rsvp from './components/Rsvp'
 import Confirmation from './components/Confirmation'
+import Carousel from './components/Carousel'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/details' element={<Details />} />
         <Route path='/rsvp' element={<Rsvp />} />
         <Route path='/confirmation' element={<Confirmation />} />
+        <Route path='/carousel' element={<Carousel />} />
       </Routes>
     
     </BrowserRouter>
